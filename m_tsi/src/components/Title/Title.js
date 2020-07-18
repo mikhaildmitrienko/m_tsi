ec11d0998a2cb2503e80707981da05f1d3dc7195
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-07-18 12:34:54 -0400

    created basic frontend

---
+++ m_tsi/src/components/Title/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Title.css'
+import logo from './logo.png'
+
+function Title(){
+return(
+<div className="Title">
+    <header>
+    {/* <img scr={logo} alt="hello"/> */}
+    <h1>Ancor</h1>
+    </header>
+</div>)
+}
+
+export default Title;
