e3a3d5cb4aa7c12b643b2333e10361e9889ff2e3
CalebJKim <calebkim32@gmail.com>
2020-07-18 14:09:10 -0700

    Main Page

--- m_tsi/src/components/Title/Title.js
+++ m_tsi/src/components/Title/Title.js
@@ -5,11 +5,17 @@ import logo from './logo.png'
 function Title(){
 return(
 <div className="Title">
-    <header>
-    <img id="crab" src={logo} alt="logo"/>
-    <h1>Ancor</h1>
-    <h2>Ancor</h2>
-    </header>
+    <div>
+        <img id="crab" src={logo} alt="logo"/>
+    </div>
+    <div>
+        <header>
+            <h1>Ancor</h1>
+            <h2>Ancor</h2>
+            <h3>Our Team</h3>
+            <h4>Motto/Slogan</h4>
+        </header>
+    </div>
 </div>)
 }
 
