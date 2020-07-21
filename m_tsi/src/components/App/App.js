130587bf882dc38ff2a7a46b162a962858b44d29
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-07-21 11:51:12 -0400

    large commit

--- m_tsi/src/components/App/App.js
+++ m_tsi/src/components/App/App.js
@@ -1,14 +1,12 @@
-import React, { useEffect } from 'react';
-import logo from '../../logo.svg';
-import {Connector} from 'mqtt-react';
+import React from 'react';
 import './App.css';
-import {subscribe} from 'mqtt-react';
 import Button from '../Button/Button'
 import Title from '../Title/Title'
 import NotifBox from '../NotifBox/NotifBox'
+import Popup from 'reactjs-popup';
 
-var StringDecoder = require('string_decoder').StringDecoder;
 
+require('dotenv').config({path: '../.env'});
 
 class App extends React.Component {
 
@@ -16,9 +14,51 @@ class App extends React.Component {
     super(props)
     this.eventSource = new EventSource(`http://localhost:5000/feed`);
     this.state = {
+      collect: false,
       postureStatus: 'Good',
       stressStatus: 'Good',
+      warningCount: 0,
+      popUpState: false
     };
+  }
+
+  reminderLogic(topic, message){
+    if(topic === 'IsitaT03/feeds/posture'){
+      this.setState({postureStatus: message})
+
+      switch(message){
+        case 'Warning':
+          this.setState({warningCount: this.state.warningCount + 1})
+          if(this.state.warningCount >= 5){
+            this.setState({popUpState: true})
+          }
+          break;
+        case 'Bad':
+          this.setState({postureStatus: message, warningCount: 0, popUpState:true})
+          // alert('Lean back, your posture is really bad')
+
+          break;
+        case 'Good':
+          this.setState({warningCount: 0})
+          break;
+        }
+
+    }else if(topic === 'mdmit/feeds/test_feed2'){
+
+      let previousState = this.state.stressStatus;
+      switch(message){
+        default: 
+          this.setState({stressStatus: message})
+          console.log('Here')
+          break;
+        case 'Bad':
+          this.setState({stressStatus: message});
+          if(previousState === 'Bad'){
+            alert('Take a breather')
+          }
+      }
+      
+    }
   }
 
   handleStream() {
@@ -26,23 +66,14 @@ class App extends React.Component {
       let responseJSON = JSON.parse(e.data);
       let topic = Buffer.from(responseJSON['topic']).toString()
       let message = Buffer.from(responseJSON['message']).toString()
-
-      if(topic === 'mdmit/feeds/test_feed'){
-        if(this.state.postureStatus !== message){
-        this.setState({postureStatus: message})
-        }
-      }else if(topic === 'mdmit/feeds/test_feed2'){
-        if(this.state.stressStatus !== message){
-        this.setState({stressStatus: message})
-        }
-      }
+      this.reminderLogic(topic, message)
     }
   }
 
-
-
+  
   componentDidMount() {
     this.handleStream()
+
   }
 
   componentWillUnmount() {
@@ -51,14 +82,21 @@ class App extends React.Component {
      }
   }
 
+  toggleCollect() {
+    if(this.state.collect === false){
+      this.setState({collect: true})
+    }else{
+      this.setState({collect: false})
+    }
+  }
 
   render(){
+
     return(
 
     <div className="App">
     <Title/>
       <div className="App-header">
-
       <Button/>
       </div>
 
@@ -66,10 +104,16 @@ class App extends React.Component {
         <NotifBox title="Posture" status={this.state.postureStatus}/>
         <NotifBox title="Stress" status={this.state.stressStatus}/>
       </div> 
+      <Popup open={this.state.popUpState} modal>
+        <h1>Energize!</h1>
+        <h2>You've been sitting for a while now, <br/> so let's energize!</h2>
+        
+        <h3>Please roll your neck for <em> 5 minutes</em></h3>
+      </Popup>
+
     </div>)
   }
   
-
 }
 
 export default App;
